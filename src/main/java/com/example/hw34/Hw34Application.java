58f26621df3d425db424ee27872192a79a840975
Evllooew <evllooev@icloud.com>
2024-01-04 11:33:22 +0300

    дз готово

---
+++ src/main/java/com/example/hw34/Hw34Application.java
@@ -0,0 +1,13 @@
+package com.example.hw34;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hw34Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hw34Application.class, args);
+	}
+
+}
